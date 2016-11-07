318e11b12557a194cc7ec8ec010b12ed0db45dfc
Igor <ic69@yandex.ru>
2016-11-08 01:22:19 +0300

    2

--- app/src/main/java/ru/iconstanta/dzin/MainActivity.java
+++ app/src/main/java/ru/iconstanta/dzin/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.iconstanta.dzin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.SharedPreferences;
@@ -8,25 +9,33 @@ import android.view.View;
 import android.widget.Switch;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.text.format.Time;
 
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences mSettings;
     public static final String APP_PREFERENCES = "dzin_settings";
     public static final String APP_PREFERENCES_IsActive = "IsActive";
-    public static final String APP_PREFERENCES_Start = "Start";
-    public static final String APP_PREFERENCES_Stop = "Stop";
-    public static final String APP_PREFERENCES_Period = "Period";
+    public static final String APP_PREFERENCES_StartHour = "StartHour";
+    public static final String APP_PREFERENCES_StartMinute = "StartMinute";
+    public static final String APP_PREFERENCES_StopHour = "StopHour";
+    public static final String APP_PREFERENCES_StopMinute = "StopMinute";
+    public static final String APP_PREFERENCES_PeriodHour = "PeriodHour";
+    public static final String APP_PREFERENCES_PeriodMinute = "PeriodMinute";
+    private static final int START_TIME_PICKER = 0;
 
     private boolean mIsActive;
 
     private Switch mActiveSwitch;
     private TextView mStartTextLabel;
-    private TextClock mStartTextClock;
+    private TextView mStartTextClock;
     private TextView mStopTextLabel;
-    private TextClock mStopTextClock;
+    private TextView mStopTextClock;
     private TextView mPeriodTextLabel;
-    private TextClock mPeriodTextClock;
+    private TextView mPeriodTextClock;
+
+    private Time mStartTime;
+    private Time mStopTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +45,12 @@ public class MainActivity extends AppCompatActivity {
         mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         mActiveSwitch = (Switch) findViewById(R.id.ActiveSwitch);
         mStartTextLabel = (TextView) findViewById(R.id.StartTextLabel);
-        mStartTextClock = (TextClock) findViewById(R.id.StartTextValue);
+        mStartTextClock = (TextView) findViewById(R.id.StartTextValue);
         mStopTextLabel = (TextView) findViewById(R.id.StopTextLabel);
-        mStopTextClock = (TextClock) findViewById(R.id.StopTextValue);
+        mStopTextClock = (TextView) findViewById(R.id.StopTextValue);
         mPeriodTextLabel = (TextView) findViewById(R.id.PeriodTextLabel);
-        mPeriodTextClock = (TextClock) findViewById(R.id.PeriodTextValue);
+        mPeriodTextClock = (TextView) findViewById(R.id.PeriodTextValue);
+        mStartTime = new Time();
     }
 
     @Override
@@ -49,8 +59,12 @@ public class MainActivity extends AppCompatActivity {
         if (mSettings.contains(APP_PREFERENCES_IsActive)) {
             // Получаем число из настроек
             mIsActive = mSettings.getBoolean(APP_PREFERENCES_IsActive, true);
+
+            mStartTime.hour = mSettings.getInt(APP_PREFERENCES_StartHour, 13);
+            mStartTime.minute = mSettings.getInt(APP_PREFERENCES_StartMinute, 0);
             // Выводим на экран данные из настроек
             mActiveSwitch.setChecked(mIsActive);
+            mStartTextClock.setText(mStartTime.format("%H:%M"));
             SetEnabled();
         }
     }
@@ -61,7 +75,21 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences.Editor editor = mSettings.edit();
         editor.putBoolean(APP_PREFERENCES_IsActive, mIsActive);
+        editor.putInt(APP_PREFERENCES_StartHour, mStartTime.hour);
+        editor.putInt(APP_PREFERENCES_StartMinute, mStartTime.minute);
         editor.apply();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == START_TIME_PICKER) {
+            if (resultCode == RESULT_OK) {
+                mStartTime.hour = data.getIntExtra("Hour", 13);
+                mStartTime.minute = data.getIntExtra("Minute", 0);
+                mStartTextClock.setText(mStartTime.format("%H:%M"));
+            }
+        }
     }
 
     public void onActiveChanged(View view) {
@@ -70,6 +98,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStartClick(View view) {
+        Intent intent = new Intent(MainActivity.this, TimePickerActivity.class);
+        intent.putExtra("Hour", mStartTime.hour);
+        intent.putExtra("Minute", mStartTime.minute);
+        startActivityForResult(intent, START_TIME_PICKER);
     }
 
     protected void SetEnabled() {
--- app/src/main/java/ru/iconstanta/dzin/TimePickerActivity.java
+++ app/src/main/java/ru/iconstanta/dzin/TimePickerActivity.java
@@ -1,13 +1,43 @@
 package ru.iconstanta.dzin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
 
 public class TimePickerActivity extends AppCompatActivity {
+    private boolean mIsActive;
+
+    private TimePicker mTimePicker;
+    private int mHour;
+    private int mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_picker);
+        mTimePicker = (TimePicker) findViewById(R.id.time_picker);
+        mTimePicker.setIs24HourView(true);
+        mHour = getIntent().getExtras().getInt("Hour");
+        mMinute = getIntent().getExtras().getInt("Minute");
+
+        mTimePicker.setCurrentHour(mHour);
+        mTimePicker.setCurrentMinute(mMinute);
+    }
+
+    public void onCancel(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    public void onOK(View view) {
+        mHour = mTimePicker.getCurrentHour();
+        mMinute= mTimePicker.getCurrentMinute();
+        Intent answerIntent = new Intent();
+        answerIntent.putExtra("Hour", mHour);
+        answerIntent.putExtra("Minute", mMinute);
+        setResult(RESULT_OK, answerIntent);
+        finish();
     }
 }
