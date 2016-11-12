09dd917c58ad03ad76a1ed0588bdbc3809089516
Igor <ic69@yandex.ru>
2016-11-12 23:23:58 +0300

    3

---
+++ app/src/main/java/ru/iconstanta/dzin/AlarmManagerBroadcastReceiver.java
@@ -0,0 +1,55 @@
+package ru.iconstanta.dzin;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.os.Bundle;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.widget.Toast;
+/**
+ * Created by Igor on 12.11.2016.
+ */
+
+public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String ONE_TIME="onetime";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "YOUR TAG");
+//Осуществляем блокировку
+        wl.acquire();
+
+//Здесь можно делать обработку.
+        Bundle extras = intent.getExtras();
+        StringBuilder msgStr = new StringBuilder();
+
+        if (extras != null && extras.getBoolean(ONE_TIME, Boolean.FALSE)) {
+//проверяем параметр ONE_TIME, если это одиночный будильник,
+//выводим соответствующее сообщение.
+            msgStr.append("Одноразовый будильник: ");
+        }
+        Format formatter = new SimpleDateFormat("hh:mm:ss a");
+        msgStr.append(formatter.format(new Date()));
+
+        Toast.makeText(context, msgStr, Toast.LENGTH_LONG).show();
+
+//Разблокируем поток.
+        wl.release();
+    }
+
+    public void setOnetimeTimer(Context context){
+        AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
+        intent.putExtra(ONE_TIME, Boolean.TRUE);//Задаем параметр интента
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), pi);
+    }
+}
